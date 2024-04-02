06c1fcd8cde8ded2d64fac7313848ae407e44ab6
Houssam-Eddine AL KADERI <alkaderihoussam@gmail.com>
2024-04-02 12:20:37 +0000

    none

--- Recipe-Book-Backend/src/main/java/com/example/recipebookbackend/RecipeBookBackendApplication.java
+++ Recipe-Book-Backend/src/main/java/com/example/recipebookbackend/RecipeBookBackendApplication.java
@@ -17,7 +17,6 @@ public class RecipeBookBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeBookBackendApplication.class, args);
-        System.out.println("hello a wlad nass");
     }
     @Bean
     public CommandLineRunner initializeData(IngredientService ingredientService, RecipeService recipeService) {
@@ -27,16 +26,13 @@ public class RecipeBookBackendApplication {
             ingredients.add(new Ingredient("Ingredient1", 100));
             ingredients.add(new Ingredient("Ingredient2", 50));
             ingredients.add(new Ingredient("Ingredient3", 200));
-
             // Save ingredients
             ingredientService. saveAllIngredients(ingredients);
-
             // Example data for recipes
             List<Recipe> recipes = new ArrayList<>();
             recipes.add(new Recipe("Recipe1", "Description1", new ArrayList<>()));
             recipes.add(new Recipe("Recipe2", "Description2", new ArrayList<>()));
             recipes.add(new Recipe("Recipe3", "Description3", new ArrayList<>()));
-
             // Save recipes
             recipeService.saveAllRecipes(recipes);
         };
